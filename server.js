9fb0029712ce1a3b4e065788f78eeaea6ec30f0c
jonesjonathan <jonesjon@oregonstate.edu>
2017-05-30 11:33:26 -0700

    Added default path to website if no specific url is provided.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var app = express();
 
 app.use('/index.html', express.static('public'));
 app.use('/damgur', express.static('public'));
+app.use(express.static('public'));
 
 app.get('*', function(req, res){
 	res.status(404).send('404');
