c48f358f29874ecbe127963015c8bccf9735a448
Taegan Warren <taeganwarren@users.noreply.github.com>
2017-06-06 16:14:20 -0700

    Added express-handlebars support to dynamically generate FAQ page

--- server.js
+++ server.js
@@ -4,31 +4,33 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 var port = process.env.PORT || 3000;
 
+var faqData = require('./faqData');
+
 var app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
-
 app.use('/damgur', express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.get(/\/(index([.]html)?)?$/, function (req, res, next) {
-
   var templateArgs = {
-    searchBar: 'visible'
+    searchBar: true
   };
-
   res.render('index', templateArgs);
 });
 
-app.get(/\/faq([.]html)?/, function (req, res, next) {
-  res.render('faq');
+app.get(/\/(faq([.]html)?)?$/, function (req, res, next) {
+  var template_arguments = {
+    faqs: faqData,
+    show: true,
+    searchBar: false
+  };
+  res.render('faqPage', template_arguments);
 });
 
-
 app.get('*', function(req, res){
-  res.status(404).send('404');
+  res.status(404).render('404Page');
 });
 
 app.listen(port, function(){
