46a928394c1de910766ee60a911b25169201edcc
jonesjonathan <jonesjon@oregonstate.edu>
2017-06-08 14:33:15 -0700

    Added some more JS functionality to the add-meme stuff.

--- public/script.js
+++ public/script.js
@@ -1,11 +1,30 @@
-
 window.onload=function(){
 
+	var addButton = document.getElementById("add-meme-button");
+	var newMemeCancel = document.getElementById("new-meme-cancel");
 	var link = document.getElementById("new-meme-link");
+
 	link.addEventListener('keyup', displayImg);
+	addButton.addEventListener('click', addMeme);
+	newMemeCancel.addEventListener('click', exitMemeAdder);
 
 	function displayImg(){
 		var img = document.getElementById("new-meme-image");
 		img.src = link.value;
+	}
+
+	function addMeme() {
+		document.getElementById("new-meme-backdrop").classList.remove("hidden");
+		document.getElementById("new-meme").classList.remove("hidden");
+		document.getElementById("new-meme-button-container").classList.remove("hidden");
+	}
+
+	function exitMemeAdder() {
+		document.getElementById("new-meme-backdrop").classList.add("hidden");
+		document.getElementById("new-meme").classList.add("hidden");
+		document.getElementById("new-meme-button-container").classList.add("hidden");
+
+		document.getElementById("new-meme-link").value="";
+		document.getElementById("new-meme-subtitle").value="";
 	}
 }
