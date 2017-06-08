7f095f547ab8b6e2437dd6f051b54a80d0c626fa
jonesjonathan <jonesjon@oregonstate.edu>
2017-06-08 13:46:50 -0700

    Changed close button to an actual button. Added an ADD MEME button to the top right of screen. No JS yet.

--- public/script.js
+++ public/script.js
@@ -1,12 +1,11 @@
 
-
 window.onload=function(){
-	
-var link = document.getElementById("new-meme-link");
-link.addEventListener('keyup', displayImg);
 
-function displayImg(){
-	var img = document.getElementById("new-meme-image");
-	img.src = link.value;
-}
+	var link = document.getElementById("new-meme-link");
+	link.addEventListener('keyup', displayImg);
+
+	function displayImg(){
+		var img = document.getElementById("new-meme-image");
+		img.src = link.value;
+	}
 }
