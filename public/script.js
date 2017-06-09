ff142bf3526c4b7e7238ceccc3fd3242aae9132f
vandomej <vandomej@oregonstate.edu>
2017-06-09 00:53:41 -0700

    Added comments to javascript code

--- public/script.js
+++ public/script.js
@@ -1,12 +1,15 @@
 window.onload=function(){
+	//Container that holds all of the memes
 	var memeContainer = document.querySelector('main.photo-container');
 
+	//Getting various specific elements from the page
 	var addButton = document.getElementById("add-meme-button");
 	var newMemeCancel = document.getElementById("new-meme-cancel");
 	var link = document.getElementById("new-meme-link");
 	var searchBar = document.getElementById("navbar-searchBar");
 	var deleteButton = document.getElementsByClassName('close-button');
-
+	
+	//Connecting various event listeners to each of the elements collected above
 	link.addEventListener('keyup', displayImg);
 	addButton.addEventListener('click', addMeme);
 	newMemeCancel.addEventListener('click', exitMemeAdder);
@@ -15,17 +18,20 @@ window.onload=function(){
 		deleteButton[i].addEventListener('click', deleteMeme);
 	}
 
+	//I assumed that when you press any key while selecting the new meme link, it will display an image?
 	function displayImg(){
 		var img = document.getElementById("new-meme-image");
 		img.src = link.value;
 	}
 
+	//When this function calls, it stops the modal from being hidden, and displays it to the user
 	function addMeme() {
 		document.getElementById("new-meme-backdrop").classList.remove("hidden");
 		document.getElementById("new-meme").classList.remove("hidden");
 		document.getElementById("new-meme-button-container").classList.remove("hidden");
 	}
 
+	//resets the modal when the user hits cancel
 	function exitMemeAdder() {
 		document.getElementById("new-meme-image").src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/-Insert_image_here-.svg/2000px--Insert_image_here-.svg.png";
 		document.getElementById("new-meme-link").value="";
@@ -34,15 +40,22 @@ window.onload=function(){
 		document.getElementById("new-meme").classList.add("hidden");
 		document.getElementById("new-meme-button-container").classList.add("hidden");
 	}
+
+	//When entering a term into the searchbar, it filters through all of the memes on the page and displays ones relevant
 	function filterMeme() {
+		//Collects the string from the search bar
 		var searchString = searchBar.value.trim().toUpperCase();
+
 		if (memeContainer.hasChildNodes()) {
+
+			//Obtains all of the memes on the webpage and loops through them
 			var memeArray = memeContainer.getElementsByTagName('article');
 			var caption;
 
 			for (var i = 0; i < memeArray.length; i++) {
 				caption = memeArray[i].querySelector('p.photo-subtitle').textContent.trim().toUpperCase();
 
+				//checks if the current meme has content relevant to the search
 				if (caption.indexOf(searchString) !== -1) {
 					memeArray[i].classList.remove('hidden');
 				}
@@ -52,13 +65,18 @@ window.onload=function(){
 			}
 		}
 	}
+
+	//When clicking the x button on a meme, this will delete the meme.
 	function deleteMeme(event) {
+		//Gets info associated with the current meme being clicked on.
 		var photoElement = event.target.parentElement.parentElement;
 		var photoID = photoElement.querySelector('a.comment-link').getAttribute('href');
 
+		//Starts a new delete type request.
 		var deleteRequest = new XMLHttpRequest();
 		deleteRequest.open('DELETE', photoID, true);
 		
+		//Callback function, used when the request has finished and a response has been sent.
 		deleteRequest.onload = function(event) {
 			if (deleteRequest.readyState == 4) {
 				if (deleteRequest.status == 200) {
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//Importing needed components
 var path = require('path');
 var fs = require('fs');
 var express = require('express');
@@ -7,46 +8,84 @@ var contributorData = require('./contributorData');
 var photoData = require('./photoData');
 var port = process.env.PORT || 3000;
 
+//Starting our application as an express application
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+//Initializing the engines used by the app.
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+//Making all static files route to public
+app.use(express.static(path.join(__dirname, 'public')));
+
+//The get method that is called for any of the following URL's
+//  "/"
+//  "/index"
+//  "/index.html"
+//  Used to view the index, or home page for damgur
 app.get(/\/(index([.]html)?)?$/, function (req, res, next) {
-  var templateArgs = {
+  
+  //Setting the template arguments
+  var template_arguments = {
     photos: photoData,
     searchBar: true,
     style: "./style.css",
     addButton: true,
     newMeme: true
   };
-  res.render('index', templateArgs);
+
+  //Rendering the index.handlebars page
+  res.render('index', template_arguments);
 });
 
-app.get(/\/(faq([.]html)?)?$/, function (req, res, next) {
+//The get method that is called for any of the following URL's
+//  "/faq"
+//  "/faq.html"
+//  Used to view the faq page for damgur
+app.get(/\/faq([.]html)?$/, function (req, res, next) {
+  
+  //Setting up the template arguments		
   var template_arguments = {
     faqs: faqData,
     searchBar: false,
     style: "./style.css"
   };
+
+  //Rendering the faqPage.handlebars page
   res.render('faqPage', template_arguments);
 });
 
-app.get(/\/(contributors([.]html)?)?$/, function (req, res, next) {
+//The get method that is called for any of the following URL's
+//  "/contributors"
+//  "/contributors.html"
+//  Used to view the contributors page for damgur
+app.get(/\/contributors([.]html)?$/, function (req, res, next) {
+
+  //Setting up the template arguments
   var template_arguments = {
     contributors: contributorData,
     searchBar: false,
     style: "./style.css"
   };
+
+  //Rendering the contributorPage.handlebars page
   res.render('contributorPage', template_arguments);
 });
 
+//The get method that is called for any of the following URL's
+//  "/####"
+//  Used to view the comments page for a specific meme image
 app.get('/:num', function (req, res, next) {
+  
+  //Retreives the photo information from the photoData json file
   var num = req.params.num;
   var photoDex = photoData[num];
   var commentDex = photoData[num].comments;
+
+  //If the photo exists
   if(photoDex) {
+
+	//Setting up the template arguments
     var template_arguments = {
       photo: photoDex,
       addButton: false,
@@ -55,18 +94,21 @@ app.get('/:num', function (req, res, next) {
       style: "./commentStyle.css"
     };
 
-    console.log(template_arguments);
-
+    //Rendering the commentPage.handlebars page
     res.render('commentPage', template_arguments);
   } else {
     next();
   }
 });
 
+//The delete method that is called for any of the following URL's
+//  ""/####""
+//  Used when clicking the x button on the top right of an image, causing it to be deleted from the website
 app.delete('/:num', function(req, res, next) {
+
+  //Obtaining the photo data from the photoData json file
   var photoID = '/' + req.params.num;
   var photo;
-
   for (var i = 0; i < photoData.length; i++) {
     if (photoData[i].index.trim() === photoID.trim()) {
       photo = photoData[i];
@@ -75,9 +117,12 @@ app.delete('/:num', function(req, res, next) {
     }
   }
 
+  //Sending back a 200 code when the deletion is finished errorlessly
   res.status(200).send();
 });
 
+//The get method that is called when a url route does not match any of the above routes, or the static files in public.
+//Generates the 404 page, as the page requested could not be found.
 app.get('*', function(req, res){
   var template_arguments = {
     style: "./style.css"
@@ -85,6 +130,7 @@ app.get('*', function(req, res){
   res.status(404).render('404Page', template_arguments);
 });
 
+//Starting the server by booting up a host on the given port
 app.listen(port, function(){
   console.log('Server listening on port ' + port);
 });
